9fee1f7739f8e1f5bab6e123391803851226c2ca
lipcoder <lipcoder@foxmail.com>
2026-03-09 19:24:23 +0800

    range详细用法

--- gouse/02.基本数据/for-range/main.go
+++ gouse/02.基本数据/for-range/main.go
@@ -4,37 +4,37 @@ import (
 	"fmt"
 )
 
+// range 输出右边这个数组/切片的索引和值
 func main() {
-	notes := [7]string{"do", "re", "mi", "fa", "so", "la", "xi"}
-	// 输出数组里面的 for循环1
+	notes := [7]int{0, 1, 2, 3, 4, 5, 6}
 	fmt.Println("len(notes)=", len(notes))
+
 	for i := 0; i < len(notes); i++ {
 		v := notes[i]
-		fmt.Print(i, v, " ")
+		fmt.Print(v)
 	}
 	fmt.Println()
 
-	// range 输出右边这个数组/切片的索引和值
-	// 这里面判断终止的条件是len(notes)
-	/*	for _,v := range s{
-			...
-		}
-	// 本质上上面与下面是同一种写法	
-		for i := 0; i < len(s); i++ {
-	    	v := s[i]
-	    	...
-		}
-	*/
-	
-	for index, note := range notes {
-		fmt.Print(index, note, " ")
+	for i := range len(notes) {
+		fmt.Print(notes[i])
+	}
+	fmt.Println()
+
+	for i, note := range notes {
+		fmt.Printf("%d%d ",i,note)
 	}
 	fmt.Println()
 
 	for _, note1 := range notes {
-		fmt.Print(note1, " ")
+		fmt.Print(note1)
 	}
-	// go里面要求所有返回值都要被接收，但是又要求所有变量都要被使用，所以有了_，接收但直接抛弃
+	fmt.Println()
+
+	for range len(notes) {
+		fmt.Print(1)
+	}
+	// go里面要求所有返回值都要被接收，但是又要求所有变量都要被使用，
+	// 所以有了_ 用来占位并显式丢弃一个值
 }
 
 // range除了可以遍历数组，切片还能遍历map
