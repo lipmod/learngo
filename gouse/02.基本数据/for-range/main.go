411141051476aea8d9a5dc65d79801c631452207
lipcoder <lipcoder@foxmail.com>
2026-03-09 19:24:51 +0800

    nothing

--- gouse/02.基本数据/for-range/main.go
+++ gouse/02.基本数据/for-range/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// range除了可以遍历数组，切片还能遍历map
 // range 输出右边这个数组/切片的索引和值
 func main() {
 	notes := [7]int{0, 1, 2, 3, 4, 5, 6}
@@ -21,7 +22,7 @@ func main() {
 	fmt.Println()
 
 	for i, note := range notes {
-		fmt.Printf("%d%d ",i,note)
+		fmt.Printf("%d%d ", i, note)
 	}
 	fmt.Println()
 
@@ -36,5 +37,3 @@ func main() {
 	// go里面要求所有返回值都要被接收，但是又要求所有变量都要被使用，
 	// 所以有了_ 用来占位并显式丢弃一个值
 }
-
-// range除了可以遍历数组，切片还能遍历map
