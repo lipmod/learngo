8599a5f60829f6aee677084158d8ca0655ce67b5
lipcoder <lipcoder@foxmail.com>
2026-03-21 19:22:26 +0800

    nothing

---
+++ gouse/05.方法/方法声明/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "math"
+
+type Point struct{ x, y float64 }
+
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.x-p.x, q.y-p.y)
+}
+
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.x-p.x, q.y-p.y)
+}
